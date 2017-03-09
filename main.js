2db2a125c1b3dc7197b5b7f48db997dcbaedc1b0
Ryan Apellanes <ryan.apellanes@gmail.com>
2017-03-08 16:07:40 -0800

    latest changes to main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var the_number = null;
 
 function pick_number(){ //declared function
-    var random_number = Math.floor(Math.random() * 10) + 1; // inclusive at both min and max
+    var random_number = Math.floor(Math.random() * (10) + 1); // inclusive at both min and max
     the_number = random_number;
     return random_number;
 }
@@ -13,7 +13,7 @@ $(document).ready(function(){ //click handler
 });
 
 function make_guess(){ //declared function
-    var the_guess = parseInt($("#guess_input").val()); //val() will get value from user input field
+    var the_guess = parseInt($("#guess_input").val(), 10); //val() will get value from user input field
     if (the_guess > the_number){
         $("#response_div").text("Too High!");
     } else if (the_guess < the_number){
